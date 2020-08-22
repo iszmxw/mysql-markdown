b2018aba9c750caf57f97392fd011afdc290f390
追梦小窝 <mail@54zm.com>
2020-08-22 23:17:33 +0800

    优化静态资源

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
 <?php
-//
-//require_once './vendor/autoload.php'; // 加载自动加载文件
-//use Iszmxw\MysqlMarkdown\Mysql;
-//
-//// 配置数据库参数
-//$config = [
-//    'dbs'      => 'mysql',
-//    'host'     => '127.0.0.1',
-//    'port'     => 3306,
-//    'charset'  => 'utf8',
-//    'name'     => 'basicsapi_test',
-//    'user'     => 'basicsapi_test',
-//    'password' => 'basicsapi_test',
-//];
-//Mysql::markdown($config);
+
 require_once './vendor/autoload.php'; // 加载自动加载文件
 use Iszmxw\MysqlMarkdown\Mysql;
 
 // 配置数据库参数
 $config = [
     'dbs'      => 'mysql',
-    'host'     => '10.10.10.115',
+    'host'     => '192.168.26.134',
     'port'     => 3306,
     'charset'  => 'utf8',
-    'name'     => 'tftpay',
-    'user'     => 'yjt',
-    'password' => 'Yjt@2019',
+    'name'     => 'basicsapi_test',
+    'user'     => 'root',
+    'password' => 'root',
 ];
-
-$result = [];
 Mysql::markdown($config);
